70433f769df7b3e52284bf88537acb2bec0a1a39
George <109150570+Nok1on1@users.noreply.github.com>
2024-06-02 14:33:08 +0400

    .

---
+++ gametest/target/debug/deps/gametest-02f14ba2ee6e2fd9.d
@@ -0,0 +1,5 @@
+/home/noklon/RUST-projects/gametest/target/debug/deps/libgametest-02f14ba2ee6e2fd9.rmeta: src/main.rs
+
+/home/noklon/RUST-projects/gametest/target/debug/deps/gametest-02f14ba2ee6e2fd9.d: src/main.rs
+
+src/main.rs:
---
+++ gametest/target/debug/deps/gametest-b6ccb08a977b6163.d
@@ -0,0 +1,5 @@
+/home/noklon/RUST-projects/gametest/target/debug/deps/libgametest-b6ccb08a977b6163.rmeta: src/main.rs
+
+/home/noklon/RUST-projects/gametest/target/debug/deps/gametest-b6ccb08a977b6163.d: src/main.rs
+
+src/main.rs:
